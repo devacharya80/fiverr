8ed3a0c1957d8dc7bd3e9e7689d609d26388b46d
devacharya80 <dhanudeva80@gmail.com>
2026-03-11 15:19:09 +0530

    fake-data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const methodOverride = require("method-override");
 const path = require("path");
 const mongoose = require("mongoose");
+const gigSchema = require("./models/gig");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -16,9 +17,20 @@ app.get("/", (req, res) => {
   res.send("Fiverr App Working");
 });
 
+// Fake data
+const sampleGig = new gigSchema({
+  title: "Build a MERN website",
+  description: "I will build a full stack website",
+  price: 5000,
+  category: "web dev",
+});
+sampleGig.save().then(() => {
+  console.log("saved");
+});
+
 // Port defining and DB connection
 mongoose
-  .connect("mongodb://127.0.0.1:27017/test")
+  .connect("mongodb://127.0.0.1:27017/fiverr")
   .then(() => {
     app.listen(8080, () => {
       console.log("Listening and connected to DB");
--- models/gig.js
+++ models/gig.js
@@ -1,38 +1,41 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const gigSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
+const gigSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
 
-  description: {
-    type: String,
-    required: true,
-  },
+    description: {
+      type: String,
+      required: true,
+    },
 
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
 
-  category: {
-    type: String,
-    required: true,
-    enum: ["web dev", "aiml"],
-  },
+    category: {
+      type: String,
+      required: true,
+      enum: ["web dev", "aiml"],
+    },
 
-  image: {
-    type: String,
-    default:
-      "https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3407.jpg",
-    set: (v) =>
-      v === ""
-        ? "https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3407.jpg"
-        : v,
+    image: {
+      type: String,
+      default:
+        "https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3407.jpg",
+      set: (v) =>
+        v === ""
+          ? "https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3407.jpg"
+          : v,
+    },
   },
-})({ timestamps: true });
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("Gig", gigSchema);
